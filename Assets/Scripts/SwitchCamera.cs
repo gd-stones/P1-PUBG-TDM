e200c2eae3ec3a92ce3f3c1efcea432694543c22
gd-stones <ngducviet2105@gmail.com>
2023-11-25 19:59:42 +0700

    customize cam position

--- Assets/Scripts/SwitchCamera.cs
+++ Assets/Scripts/SwitchCamera.cs
@@ -10,10 +10,18 @@ public class SwitchCamera : MonoBehaviour
     public GameObject thirdPersonCam;
     public GameObject thirdPersonCanvas;
 
+    [Header("Camera animtor")]
+    public Animator animator;
+
     private void Update()
     {
         if (Input.GetButton("Fire2") && Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
+            animator.SetBool("Idle", false);
+            animator.SetBool("IdleAim", true);
+            animator.SetBool("AimWalk", true);
+            animator.SetBool("Walk", true);
+
             thirdPersonCam.SetActive(false);
             thirdPersonCanvas.SetActive(false);
             aimCam.SetActive(true);
@@ -21,6 +29,11 @@ public class SwitchCamera : MonoBehaviour
         }
         else if (Input.GetButton("Fire2"))
         {
+            animator.SetBool("Idle", false);
+            animator.SetBool("IdleAim", true);
+            animator.SetBool("AimWalk", false);
+            animator.SetBool("Walk", false);
+
             thirdPersonCam.SetActive(false);
             thirdPersonCanvas.SetActive(false);
             aimCam.SetActive(true);
@@ -28,6 +41,10 @@ public class SwitchCamera : MonoBehaviour
         }
         else
         {
+            animator.SetBool("Idle", true);
+            animator.SetBool("IdleAim", false);
+            animator.SetBool("AimWalk", false);
+
             thirdPersonCam.SetActive(true);
             thirdPersonCanvas.SetActive(true);
             aimCam.SetActive(false);
